fe5b3256ade484744fab9affdcc11be3d3d27fe5
HarshitSh-25 <71830227+HarshitSh-25@users.noreply.github.com>
2023-08-20 00:09:59 +0530

    Added server

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+dotenv.config()
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+// const notFoundMiddleware = require('../server/middleware/not-found');
+
+const userRoutes = require('./routes/userRoutes.js');
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(morgan('dev'));
+
+app.use('/v1/user', userRoutes);
+
+// app.use(notFoundMiddleware)
+
+// Cross Origin middleware
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
+const port = 8000;
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(8000, () => {
+            console.log(`Server started on port: ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+});
