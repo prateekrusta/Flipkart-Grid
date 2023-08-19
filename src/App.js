5db185063d64888d916d075da0fc966e54a0acc3
Prateek Rustagi <prateekrustagi2001@gmail.com>
2023-08-19 23:06:21 +0530

    heyhey

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import MainHomepage from './components/homepage/main-homepage';
 
 function App() {
   return (
     <div className="App">
-     
+       <BrowserRouter>
+            <main>
+              <Routes>
+                <Route path="/" element={<MainHomepage />} />
+              </Routes>
+            </main>
+       </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/homepage/banner.js
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Banner;
